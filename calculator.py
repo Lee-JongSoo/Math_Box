80dd79c5ab7a18cd6e932715ede03190ae151fa2
Lee Jong Soo <jongsu2645@gmail.com>
2021-04-01 10:37:37 +0900

    Error fix

--- calculator.py
+++ calculator.py
@@ -3,12 +3,9 @@ def add(a, b):
   return a+b
 def subtract(a, b):
   return a-b
-<<<<<<< HEAD
-
+  
 def multiply(a, b):
 	return a*b
 	
 def divide_free(a, b):  
     return a/b
-=======
->>>>>>> parent of 80e5e9f (Create README.md)
