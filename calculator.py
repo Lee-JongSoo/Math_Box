d202184a0efadd0bf70b5c25d7182dc65fc5a31f
Lee Jong Soo <jongsu2645@gmail.com>
2021-04-01 10:33:24 +0900

    Revert "Create README.md"
    This reverts commit 80e5e9f92c04e84c38df5f033af62eb9bca7ca5f.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,14 @@
 # 기본 계산기
 def add(a, b):
   return a+b
-  
 def subtract(a, b):
   return a-b
+<<<<<<< HEAD
 
 def multiply(a, b):
 	return a*b
 	
 def divide_free(a, b):  
     return a/b
+=======
+>>>>>>> parent of 80e5e9f (Create README.md)
